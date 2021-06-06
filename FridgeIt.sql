48d54361c0ef7ff7d1a11c121d7adf344315e36b
narrou <julien.domont123@gmail.com>
2021-06-06 19:49:39 +0200

    BDD mis a jour

--- FridgeIt.sql
+++ FridgeIt.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Jeu 03 Juin 2021 à 12:45
--- Version du serveur :  10.3.27-MariaDB-0+deb10u1
--- Version de PHP :  7.3.27-1~deb10u1
+-- Généré le :  Dim 06 Juin 2021 à 19:48
+-- Version du serveur :  5.7.27-0ubuntu0.19.04.1
+-- Version de PHP :  7.2.24-0ubuntu0.19.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `FridgeIt`
+-- Base de données :  `FridgeIT`
 --
 
 -- --------------------------------------------------------
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `t_cache` (
-  `codeBarre` int(11) NOT NULL
+  `codeBarre` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table contenant les informations des produits deja telecharg';
 
 --
@@ -61,8 +61,16 @@ CREATE TABLE `t_note` (
   `id` int(11) NOT NULL,
   `date` date NOT NULL,
   `message` text NOT NULL,
-  `auteur` text DEFAULT NULL
+  `auteur` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Contenu de la table `t_note`
+--
+
+INSERT INTO `t_note` (`id`, `date`, `message`, `auteur`) VALUES
+(1, '2021-06-03', 'Ceci est un note ', 'Julien'),
+(2, '2021-06-03', 'Ceci est une autre note', 'Samuel');
 
 -- --------------------------------------------------------
 
@@ -75,22 +83,20 @@ CREATE TABLE `t_produit` (
   `nom` text NOT NULL,
   `quantite` int(11) DEFAULT NULL,
   `peremption` date NOT NULL,
-  `codeBarre` int(11) NOT NULL,
-  `marque` text DEFAULT NULL,
-  `outside` tinyint(1) NOT NULL
+  `codeBarre` bigint(11) NOT NULL,
+  `marque` text,
+  `outside` tinyint(1) NOT NULL,
+  `en_nom` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table contenant les informations sur les produits scannés ';
 
 --
 -- Contenu de la table `t_produit`
 --
 
-INSERT INTO `t_produit` (`id`, `nom`, `quantite`, `peremption`, `codeBarre`, `marque`, `outside`) VALUES
-(4, 'Poudre', 100, '2008-07-04', 1, 'Nutella', 0),
-(5, 'Poudre', 100, '2008-07-04', 1, 'Nutella', 0),
-(6, 'Poudre', 100, '2008-07-04', 1, 'Nutella', 0),
-(7, 'Poudre', 100, '2008-07-04', 1, 'Nutella', 0),
-(8, 'Poudre', 100, '2008-07-04', 1, 'Nutella', 0),
-(9, 'Poudre', 100, '2008-07-04', 1, 'Nutella', 0);
+INSERT INTO `t_produit` (`id`, `nom`, `quantite`, `peremption`, `codeBarre`, `marque`, `outside`, `en_nom`) VALUES
+(4, 'Poudre', 100, '2008-07-04', 1, 'Nutella', 0, ''),
+(15, 'Panzani spaghetti plat', 500, '2021-05-05', 3038350208613, 'Panzani', 0, ''),
+(29, 'Barre moelleuse chocolat noir et noisettes', 0, '2021-05-05', 7622210547194, 'Not found', 0, '');
 
 --
 -- Index pour les tables exportées
@@ -129,12 +135,12 @@ ALTER TABLE `t_produit`
 -- AUTO_INCREMENT pour la table `t_note`
 --
 ALTER TABLE `t_note`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `t_produit`
 --
 ALTER TABLE `t_produit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 --
 -- Contraintes pour les tables exportées
 --
@@ -144,12 +150,6 @@ ALTER TABLE `t_produit`
 --
 ALTER TABLE `t_historique`
   ADD CONSTRAINT `FK_idProduit` FOREIGN KEY (`idProduit`) REFERENCES `t_produit` (`id`);
-
---
--- Contraintes pour la table `t_produit`
---
-ALTER TABLE `t_produit`
-  ADD CONSTRAINT `FK_codeBarre` FOREIGN KEY (`codeBarre`) REFERENCES `t_cache` (`codeBarre`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
